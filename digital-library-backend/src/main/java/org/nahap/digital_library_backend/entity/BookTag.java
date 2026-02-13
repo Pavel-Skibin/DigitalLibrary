aaf3fac077a60057cf350ebccbab3660dda91e76
Pavel-Skibin <SkibinPS05@gmail.com>
2026-02-13 19:07:53 +0300

    feat:update book metadata. Add Tag

---
+++ digital-library-backend/src/main/java/org/nahap/digital_library_backend/entity/BookTag.java
@@ -0,0 +1,26 @@
+package org.nahap.digital_library_backend.entity;
+
+import jakarta. persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "book_tags")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(exclude = {"book", "tag"})
+@ToString(exclude = {"book", "tag"})
+public class BookTag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tag_id", nullable = false)
+    private Tag tag;
+}
